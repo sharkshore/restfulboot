1536f1f40ffe6e480deaa2e1b487e9dadd5b73d1
shark <sharkshore@163.com>
2017-08-24 16:57:12 +0800

    first commit

---
+++ src/main/java/com/shark/restfulboot/RestfulbootApplication.java
@@ -0,0 +1,12 @@
+package com.shark.restfulboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulbootApplication.class, args);
+	}
+}
